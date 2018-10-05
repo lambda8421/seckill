bc266479e6189f8d604d19b9e1c15a3e9ffbe87a
yongzhao <lambda8421@163.com>
2018-10-05 12:18:18 -0400

    add seckill service

--- src/main/java/com/mofei/seckill/dao/MiaoshaUserDao.java
+++ src/main/java/com/mofei/seckill/dao/MiaoshaUserDao.java
@@ -5,8 +5,10 @@ import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import com.mofei.seckill.domain.MiaoshaUser;
+import org.springframework.stereotype.Component;
 
 @Mapper
+@Component
 public interface MiaoshaUserDao {
 	
 	@Select("select * from miaosha_user where id = #{id}")
