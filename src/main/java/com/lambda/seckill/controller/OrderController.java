051bb85edd6676a727fa24453d9c67e36aa00a7f
yongzhao <lambda8421@163.com>
2018-10-15 16:41:17 -0400

    Change the pom file

---
+++ src/main/java/com/lambda/seckill/controller/OrderController.java
@@ -0,0 +1,56 @@
+package com.lambda.seckill.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.lambda.seckill.domain.MiaoshaUser;
+import com.lambda.seckill.domain.OrderInfo;
+import com.lambda.seckill.redis.RedisService;
+import com.lambda.seckill.result.CodeMsg;
+import com.lambda.seckill.result.Result;
+import com.lambda.seckill.service.GoodsService;
+import com.lambda.seckill.service.MiaoshaUserService;
+import com.lambda.seckill.service.OrderService;
+import com.lambda.seckill.vo.GoodsVo;
+import com.lambda.seckill.vo.OrderDetailVo;
+
+@Controller
+@RequestMapping("/order")
+public class OrderController {
+
+	@Autowired
+	MiaoshaUserService userService;
+	
+	@Autowired
+	RedisService redisService;
+	
+	@Autowired
+	OrderService orderService;
+	
+	@Autowired
+	GoodsService goodsService;
+	
+    @RequestMapping("/detail")
+    @ResponseBody
+    public Result<OrderDetailVo> info(Model model,MiaoshaUser user,
+    		@RequestParam("orderId") long orderId) {
+    	if(user == null) {
+    		return Result.error(CodeMsg.SESSION_ERROR);
+    	}
+    	OrderInfo order = orderService.getOrderById(orderId);
+    	if(order == null) {
+    		return Result.error(CodeMsg.ORDER_NOT_EXIST);
+    	}
+    	long goodsId = order.getGoodsId();
+    	GoodsVo goods = goodsService.getGoodsVoByGoodsId(goodsId);
+    	OrderDetailVo vo = new OrderDetailVo();
+    	vo.setOrder(order);
+    	vo.setGoods(goods);
+    	return Result.success(vo);
+    }
+    
+}
